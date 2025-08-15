30832c30435911db9eeb465bf900d10236cb29da
Stellr <87259721+StellrVR@users.noreply.github.com>
2025-08-14 19:27:22 -0700

    Add files via upload

---
+++ src/enhancements/cursed.lua
@@ -0,0 +1,29 @@
+SMODS.Enhancement {
+    key = 'cursed',
+    pos = { x = 0, y = 0 },
+    config = {
+        extra = {
+            x_mult = 1.5
+        }
+    },
+    loc_txt = {
+        name = 'Cursed',
+        text = {
+        [1] = 'Apply {X:red,C:white}X1.5{} {C:red}Mult{}'
+    }
+    },
+    atlas = 'CustomEnhancements',
+    any_suit = false,
+    replace_base_card = false,
+    no_rank = false,
+    no_suit = false,
+    always_scores = false,
+    unlocked = true,
+    discovered = true,
+    no_collection = false,
+    calculate = function(self, card, context)
+        if context.main_scoring and context.cardarea == G.play then
+            return { x_mult = card.ability.extra.x_mult }
+        end
+    end
+}
